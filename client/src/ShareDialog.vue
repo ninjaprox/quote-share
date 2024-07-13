1656b2aadfed8671ec1d68558f77300bd9a7c282
Vinh Nguyen <ninjaprox@gmail.com>
2024-07-13 18:46:00 +0700

    chore(client): download icon on hover image

--- client/src/ShareDialog.vue
+++ client/src/ShareDialog.vue
@@ -40,8 +40,8 @@ const downloadHref = computed(() => {
     <DialogTrigger
       class="border p-1 px-2 bg-white rounded text-grass11 border-grass11"
       :style="triggerStyle"
-      >Share
-    </DialogTrigger>
+      >Share</DialogTrigger
+    >
     <DialogPortal>
       <DialogOverlay
         class="bg-blackA9 data-[state=open]:animate-overlayShow fixed inset-0 z-30"
@@ -52,15 +52,21 @@ const downloadHref = computed(() => {
         <DialogTitle class="text-mauve12 m-0 text-[17px] font-semibold"
           >Share quote</DialogTitle
         >
-        <a :href="downloadHref" download>
-          <img
-            :src="imageSrc"
-            alt=""
-            class="w-full h-auto my-2.5"
-            width="1000px"
-            height="1000px"
+        <div class="relative group">
+          <a :href="downloadHref" download>
+            <img
+              :src="imageSrc"
+              alt="quote image"
+              class="w-full h-auto my-2.5"
+              width="1000px"
+              height="1000px"
+            />
+          </a>
+          <Icon
+            icon="lucide:download"
+            class="w-8 h-8 absolute bottom-0 right-0 text-white m-2 hidden group-hover:block"
           />
-        </a>
+        </div>
         <DialogClose
           class="text-grass11 hover:bg-green4 focus:shadow-green7 absolute top-[10px] right-[10px] inline-flex h-[25px] w-[25px] appearance-none items-center justify-center rounded-full focus:shadow-[0_0_0_2px] focus:outline-none"
           aria-label="Close"
